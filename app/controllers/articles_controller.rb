a46b9783a8ef7df771261aef7352d7ea45508e24
usama010 <us74402@gmail.com>
2019-03-23 12:40:51 +0500

    Edit and show article button linked

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,41 @@
 class ArticlesController < ApplicationController
+  
+  def index
+   @article = Article.all
+  end
+
 
   def new
   	@article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 
   def create
-  	render plain: params[:article].inspect
   	@article = Article.new(article_params)
-  	@article.save
-  	redirect_to articles_show(@article)
+  	if @article.save
+  		flash[:notice] = "Article was successfuly created"
+  		redirect_to articles_path(@article)
+  	else
+  		render 'new'
+  	end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:noice] = "Article wash successfuly updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
